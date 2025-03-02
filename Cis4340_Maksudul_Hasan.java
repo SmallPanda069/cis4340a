bcb54939d6d9dc94883f255c1fbf14c231138d20
SmallPanda069 <160269954+SmallPanda069@users.noreply.github.com>
2025-03-02 01:50:19 -0500

    Add files via upload

---
+++ Cis4340_Maksudul_Hasan.java
@@ -0,0 +1,8 @@
+public class Cis4340 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from the class of cis4340 - Spring 2025 - UCF (Maksudul Hasan)");
+    }
+
+}
